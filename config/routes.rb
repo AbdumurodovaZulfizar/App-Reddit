a85afea15932e615658fd0e52db3de012e523530
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-24 14:19:18 +0500

    replace routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root "articles#index"
-  get "/articles", to: "articles#index"
-
-  get "/articles/:id", to: "articles#show"
+  resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
